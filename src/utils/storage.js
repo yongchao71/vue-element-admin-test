69f5204d50e837c65fd1a48c9878fc61951b8ea8
zhangxueyong <zhangxueyong@happyshare.com>
2019-04-03 20:49:36 +0800

    add storage

---
+++ src/utils/storage.js
@@ -0,0 +1,43 @@
+export default {
+  /**
+   * 获取本地存储
+   * @param {*} key
+   */
+  get(key) {
+    if (!key || typeof (key) !== 'string') {
+      return null
+    }
+    const result = localStorage.getItem(key) || '{}'
+    const oResult = JSON.parse(result)
+    if (oResult.expire > 0 && Date.now() > oResult.expire) {
+      return null
+    }
+    return oResult.value
+  },
+  /**
+   * 设置本地存储
+   * @param {存储key} key
+   * @param {存储值} value
+   * @param {过期时间} expire
+   */
+  set(key, value = '', expire = 0) {
+    if (!key || typeof (key) !== 'string') {
+      return false
+    }
+    const data = {
+      value: value,
+      expire: Date.now() + Number(expire)
+    }
+    localStorage.setItem(key, JSON.stringify(data))
+  },
+  /**
+   * 移除本地存储
+   * @param {*} key
+   */
+  remove(key) {
+    if (!key) {
+      return false
+    }
+    localStorage.removeItem(key)
+  }
+}
